cbaf07020b3498c1860d0ada5a74836a245b2d36
Samuel <samueloluwatommzy@gmail.com>
2018-05-13 19:32:37 +0100

    db configuration

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = {
-    database: "mongodb://localhost/dsproduct",
+    database: 'mongodb://Samueltommzy:08189513731@ds163769.mlab.com:63769/sailsdb',
     databaseName: "Dsproject",
     secretKey: "asdf"
 }
