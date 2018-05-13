20ef1ee15ab33efa4e3c00ae5707e3243d25feb7
Samuel <samueloluwatommzy@gmail.com>
2018-05-13 17:57:50 +0100

    social login included

--- public/src/app/signin/signin.component.ts
+++ public/src/app/signin/signin.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit ,Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-
+import { FormControl,Validators,FormGroup } from '@angular/forms';
 import { ApiService } from '../../app/api.service';
+import { StateService } from '@uirouter/angular';
+import { dataValidator } from '../validation';
 
 
 @Component({
@@ -11,8 +13,33 @@ import { ApiService } from '../../app/api.service';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-
-constructor() { }
+  public signinForm: FormGroup;
+  @Input() userData: any;
+  constructor(public api: ApiService, public state: StateService) { 
+    this.signinForm = new FormGroup({
+      email: new FormControl('', [Validators.compose([Validators.required,Validators.pattern(dataValidator.emailRegex)])]),
+      password: new FormControl('', [Validators.compose([Validators.required, Validators.pattern(dataValidator.passwordRegex)])])
+    })
+  }
 
   ngOnInit() { }
+
+  public login() {
+    this.userData = {
+      email: this.signinForm.controls['email'].value,
+      password: this.signinForm.controls['password'].value
+    }
+    this.api.login(this.userData).then((data)=>{
+      console.log(data.success);
+      if (!data.success) { 
+        alert(data.message);
+      }
+
+      else {
+      alert(data.message);
+      this.state.go('products' , null ,{reload: true});
+      }
+    });
+  }
 }
+
--- public/src/app/signup/signup.component.ts
+++ public/src/app/signup/signup.component.ts
@@ -2,17 +2,94 @@ import { Component, OnInit , Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service';
 import { Observable } from 'rxjs/Observable';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { StateService } from '@uirouter/angular';
+import { dataValidator } from '../validation';
+import { AuthService, FacebookLoginProvider,GoogleLoginProvider} from 'angular5-social-login';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
+
+
 export class SignupComponent implements OnInit {
-  constructor( ) {
-  
+  public signupForm: FormGroup;
+  public submitted: false;
+  @Input() userData;
+  constructor( public formbuider: FormBuilder, public state: StateService,public api: ApiService ,private social: AuthService) {
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('', [ Validators.required,Validators.minLength(3)]),
+      lastName: new FormControl('',  [ Validators.required, Validators.minLength(3)]),
+      email: new FormControl('',[Validators.compose([Validators.pattern(dataValidator.emailRegex),Validators.required])]),
+      phoneNumber: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.compose([Validators.pattern(dataValidator.passwordRegex),Validators.required])]),
+    });
    }
 
   ngOnInit() {
+       
+
   }
+  public user(){
+  let firstName = this.signupForm.controls['firstName'].value;
+  let lastName = this.signupForm.controls['lastName'].value;
+  let email = this.signupForm.controls['email'].value; 
+  let phoneNumber = this.signupForm.controls['phoneNumber'].value;
+  let password = this.signupForm.controls['password'].value;
+   this.userData = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber,
+    password: password
+  }
+
+  this.api.signup(this.userData).then((data)=>{
+    if (!data.success) { console.log("error"); alert(data.message); }
+    else{
+    console.log("new state");
+    alert(data.message);
+    this.state.go('products' , null, { reload: true});
+    }
+  });   
+  }
+
+  fbLogin() {
+    let fbProvider = FacebookLoginProvider.PROVIDER_ID;
+    this.social.signIn(fbProvider).then((data)=>{
+      console.log("data",data);
+    })
+    }
+
+
+
+googleLogin() {
+  let googleProvider = GoogleLoginProvider.PROVIDER_ID;
+    this.social.signIn(googleProvider).then((data)=>{
+      console.log("data",data);
+      let userData = {
+        name:data.name,
+        email: data.email,
+        id: data.id,
+        token: data.token
+      }
+      this.api.googleLogin(userData).then(data=>{
+        console.log(data);
+        if(!data.success){
+          alert(data.message);
+          this.state.go('signup', null, {reload:true});
+        }
+        else if(data.status == 201) {
+          alert(data.message);
+          this.state.go('products', null, {reload: true});
+        }
+        else{
+          alert(data.message);
+          this.state.go('products', null, {reload: true});
+        }
+      });
+    });
+}
 }
